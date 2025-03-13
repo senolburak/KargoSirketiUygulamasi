42060ce100b897e286a0f6707744c35555570755
BurakSenol00 <buraksenol00@outlook.com>
2025-03-13 11:15:20 +0300

    Makes tracking on the cargo number

---
+++ KargoSirketiUygulamasi/Data/KargoSirketi.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KargoSirketiUygulamasi.Data
+{
+   public class KargoSirketi
+    {
+		public delegate void DagitimMetotTemsilcisi(string no);
+
+		public event DagitimMetotTemsilcisi DagitimaCikar;
+
+		private string  _kargoNo;
+
+		public string  KargoNo
+		{
+			get { return _kargoNo; }
+			set 
+			{
+                if (value.Length != 8)
+                    throw new Exception("Kargo numarası 8 haneli olmak zorundadır");
+				if (!value.StartsWith("25"))
+					throw new Exception("25 ile başlamalıdır");
+
+                if (!int.TryParse(value.Substring(3), out int sayi))
+					throw new Exception("Son beş basamak 0-99999 arasında olmalıdır");
+
+				
+				if (value[2] != 'K')
+					throw new Exception("3. hane K olmalıdır");
+
+				if (sayi > 99999 && sayi < 0)
+					throw new Exception("Son beş hanesi 0-99999 arasında olmalıdır");
+
+				_kargoNo = value;
+				DagitimaCikar(_kargoNo);
+
+            }
+		}
+
+	}
+}
---
+++ KargoSirketiUygulamasi/Data/Nakliyat.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KargoSirketiUygulamasi.Data
+{
+    public class Nakliyat
+    {
+        public void KargoyuTasi(string kargoNo)
+        {
+            MessageBox.Show(kargoNo + "nolu Kargo adresinize teslim edilmek üzere dağıtıma çıkarılmıştır");
+        }
+    }
+}
